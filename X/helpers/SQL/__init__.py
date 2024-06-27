d210c9846c115ea1bd030904df89d0ab27e73f88
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-06-27 12:49:18 +0530

    Update __init__.py

--- X/helpers/SQL/__init__.py
+++ X/helpers/SQL/__init__.py
@@ -27,7 +27,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from X import MONGO_URL, LOGGER
+from X import LOGGER
+from config import MONGO_URl
 
 SPAMBOT = "SPAMBOT"
 
